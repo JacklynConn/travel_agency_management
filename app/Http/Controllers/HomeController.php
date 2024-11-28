4ebaad33432f112a2ec83d5bfa4c3a3dd8043d6b
makmach <471842@ntti.edu.kh>
2024-11-28 22:32:26 +0700

    11, 12 Update country display order and enhance home view with dynamic country data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $countries = Country::select()->orderBy('id', 'desc')->get();
+        $countries = Country::select()->orderBy('id', 'asc')->get();
         return view('home', compact('countries'));
     }
 }
